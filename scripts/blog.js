701f28435d2bd12c6e0ae054a0c3aae29791f26e
tonystr <tony.stroemsnaes@gmail.com>
2018-02-19 10:38:02 +0100

    blg btr

--- scripts/blog.js
+++ scripts/blog.js
@@ -25,9 +25,13 @@ function update_links($) { /// next/previous links
     page_exists(nxt_page, function(exists, url) {
         if (exists) {
             nxt_link = nxt_page;
+            let load_holder = $('<div>Failed to load content</div>');
 
-            $("#link-next").prop("href", nxt_link);
-            $("#link-next").removeClass("invisible");
+            load_holder.load(nxt_link + '.html .page-title', function() {
+                $("#link-next").prop("href", nxt_link).html('<h2>' + $("#link-next>h2").text() + ': <a id="prev-title">' + $(load_holder).text() + '</a></h2>');
+                $("#link-next").removeClass("invisible");
+            });
+
         } else {
             $("#link-next").remove();
         }
@@ -40,8 +44,12 @@ function update_links($) { /// next/previous links
 
     // Apply baxk link
     if (bck_link !== false) {
-        $("#link-prev").prop("href", bck_link);
-        $("#link-next").removeClass("invisible");
+        let load_holder = $('<div>Failed to load content</div>');
+
+        load_holder.load(bck_link + '.html .page-title', function() {
+            $("#link-prev").prop("href", bck_link).html('<h2>' + $("#link-prev>h2").text() + ': <a id="next-title">' + $(load_holder).text() + '</a></h2>');
+            $("#link-prev").removeClass("invisible");
+        });
     } else {
         $("#link-prev").remove();
     }
@@ -49,17 +57,12 @@ function update_links($) { /// next/previous links
 
 
 function include_links($, page_count) { /// Create divs with content from subpages
-    var i = 0;
+    var i = 1;
 
-    while (i < page_count + 1) {
+    while (i <= page_count) {
         page_exists(i++, function(exists, url) {
             if (exists) {
                 let page = url + ".html";
-
-                /*let load_holder = $('<div></div>');
-                load_holder.load(page + ' .page-title', function() {
-                    $('.page-title').load_holder.find('.page-title').html());
-                });
 
                 /*$.ajax({
                 let page_title = $().text();
@@ -70,8 +73,14 @@ function include_links($, page_count) { /// Create divs with content from subpag
                    }
                });*/
 
-                $("#blog-content").append( '<a class="link" id="post-' + url + '" href="' + url + '"><p> post ' + url + ' - ' + '</p></a>');
-            }
+               let load_holder = $('<div>Failed to load content</div>');
+
+               load_holder.load(page + ' .page-title', function() {
+                   /*window.alert("here");
+                   $('.page-title').load_holder.find('.page-title').html());*/
+                   $("#blog-content").append( '<a class="link" id="post-' + url + '" href="' + url + '"><p> post ' + url + ': ' + $(load_holder).text() + '</p></a>');
+               });
+           }
         });
     }
 }
