de19b2563e6643bed38835c7d9c63b5f024360be
TonyStr <tony.stroemsnaes@gmail.com>
2019-03-01 23:44:31 +0100

    build

---
+++ build/precache-manifest.71e27122ebf005c6feada8d00c5f4075.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a137f762023dab10bfc3",
+    "url": "/static/js/main.a137f762.chunk.js"
+  },
+  {
+    "revision": "618032d9c9a269bc0456",
+    "url": "/static/js/2.618032d9.chunk.js"
+  },
+  {
+    "revision": "a137f762023dab10bfc3",
+    "url": "/static/css/main.a94f8a75.chunk.css"
+  },
+  {
+    "revision": "bc82d213639eee2bbe32d5de6c2bffa1",
+    "url": "/index.html"
+  }
+];
