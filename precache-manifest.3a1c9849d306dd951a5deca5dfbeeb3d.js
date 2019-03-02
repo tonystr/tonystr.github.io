571b6ad9fd54ed1539597d4fd0153d7a9420cefb
TonyStr <tony.stroemsnaes@gmail.com>
2019-03-02 10:03:01 +0100

    Updates

---
+++ precache-manifest.3a1c9849d306dd951a5deca5dfbeeb3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2a9913c71fa151ca7b58",
+    "url": "/static/js/main.2a9913c7.chunk.js"
+  },
+  {
+    "revision": "618032d9c9a269bc0456",
+    "url": "/static/js/2.618032d9.chunk.js"
+  },
+  {
+    "revision": "2a9913c71fa151ca7b58",
+    "url": "/static/css/main.f01e88b8.chunk.css"
+  },
+  {
+    "revision": "7ba0ff6d0d318149db59b4e617cf02c8",
+    "url": "/index.html"
+  }
+];
