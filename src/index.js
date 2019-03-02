235ec7a922bfab5f2f6442e31da964dbcc71f4f7
TonyStr <tony.stroemsnaes@gmail.com>
2019-03-03 00:19:44 +0100

    Update #webdev style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom'
+import './prism.css';
 import './index.scss';
-import './highlightjs/styles/atom-one-dark.css';
 import * as serviceWorker from './serviceWorker';
 import Home from './pages/Home';
 
