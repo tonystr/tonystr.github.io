462a002473742d6da54c413468dc513d546abbf6
TonyStr <tony.stroemsnaes@gmail.com>
2019-04-08 21:54:53 +0200

    dracula window

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import './prism.css';
 import './index.scss';
 import * as serviceWorker from './serviceWorker';
-import Home from './pages/Home';
+import Home from './pages/home';
 import Article from './pages/article.js';
+import Dracula from './pages/dracula';
 import { Scrollbars } from 'react-custom-scrollbars';
 import json from './data/articles.json';
 
@@ -14,11 +15,17 @@ function ValidateArticle(props) {
     const location = (window.location.pathname.match(/\/([^/]*)$/)[1] || '').toLowerCase();
     const article = json.find(article => article.name.toLowerCase() === location);
 
-    return article ? (
-        <Article article={article} />
-    ) : (
-        <div> Could not find any article by the name "{location}" </div>
-    );
+    if (article) {
+        return <Article article={article} />;
+    } else {
+        if (location === 'dracula') {
+            return <Dracula />;
+        } else {
+            return <div> Could not find any article by the name "{location}" </div>;
+        }
+    }
+
+    // dracula
 }
 
 ReactDOM.render(
