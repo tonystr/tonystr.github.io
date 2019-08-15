e7a91c8c69a4414fa1d456537bd3cc948d27803b
TonyStr <tony.stroemsnaes@gmail.com>
2019-08-15 22:48:09 +0200

    Autotile component WIP

--- src/components/Autotiling.jsx
+++ src/components/Autotiling.jsx
@@ -1,18 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/autotile.scss';
 
 export default function Autotiling(props) {
+    const width  = props.width  || 16;
+    const height = props.height || 6;
+
+    const [drawMode, setDrawMode] = useState(null);
+    const [tiles, setTiles] = useState([]);
+
+    const startDrawing = e => setDrawMode(e.button);
+    const stopDrawing = () => setDrawMode(null);
+    const draw = e => {
+        e.target.classList.add('state-on');
+    }
 
     return (
         <Table
-            width ={props.width  || 16}
-            height={props.height || 6}
+            width={width}
+            height={height}
+            tiles={tiles}
+            onMouseDown={e => startDrawing(e)}
+            onMouseUp={e => stopDrawing(e)}
+            onMouseMove={drawMode !== null ? draw : null}
+            draw={draw}
         />
     );
 }
 
-function Table({ width, height }) {
-    const rows = [];
+function Table(props) {
+    const width  = props.width;
+    const height = props.height;
+    const rows   = [];
 
     useEffect(() => {
         const table = document.querySelector('.autotile.squares:not(.squared)');
@@ -21,15 +39,25 @@ function Table({ width, height }) {
             console.log(td.offsetWidth);
         });
         table.classList.add('squared');
-    });
+        table.oncontextmenu = () => false;
+        table.onselectstart = () => false;
+    }, []);
 
     for (let y = 0; y < height; y++) {
         const row = [];
         for (let x = 0; x < width; x++) {
-            row.push(<td></td>);
+            row.push(<td
+                className={(props.tiles[x + y * width] || {}).class}
+                onClick={props.draw}
+                onContextMenu={props.draw}
+            ></td>);
         }
         rows.push(<tr>{row}</tr>);
     }
 
-    return <table className='autotile squares'><tbody>{rows}</tbody></table>;
+    return (
+        <table {...props} className='autotile squares'>
+            <tbody>{rows}</tbody>
+        </table>
+    );
 }
