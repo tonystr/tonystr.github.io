73619c21d4c4df87db0fb85e086210af62e7c877
TonyStr <tony.stroemsnaes@gmail.com>
2019-08-17 14:07:44 +0200

    Better tile drawing

--- src/components/Autotiling.jsx
+++ src/components/Autotiling.jsx
@@ -6,40 +6,77 @@ export default function Autotiling(props) {
     const height = props.height || 6;
 
     const [drawMode, setDrawMode] = useState(null);
+    const [tiles, setTiles] = useState((() => {
+        const ts = [];
+        for (let y = 0; y < height; y++) {
+            const tr = [];
+            for (let x = 0; x < width; x++) tr.push({
+                solid: false,
+                bitflag: 0
+            });
+            ts.push(tr);
+        }
+        return ts;
+    })());
+
+    useEffect(() => {
+        const table = document.querySelector('.autotile.squares:not(.squared)');
+        table.querySelectorAll('td').forEach(td => {
+            td.style = 'height:' + td.offsetWidth + 'px';
+        });
+        table.classList.add('squared');
+        table.oncontextmenu = () => false;
+        table.onselectstart = () => false;
+    }, []);
 
     const startDrawing = e => setDrawMode(e.button);
     const stopDrawing = () => setDrawMode(null);
+    const draw = (x, y, mode = drawMode) => {
+        if (x === undefined || y === undefined || tiles[y] === undefined) return;
+        if (tiles[y][x].solid !== (mode === 0)) {
+            setTiles(prev => prev.map((row, cy) => row.map((cell, cx) => (x === cx && y === cy) ? ({
+                solid:   mode === 0,
+                bitflag: mode === 0 ? 1 : 0
+            }) : cell)));
+        }
+    }
 
     return (
-        <Table
-            width={width}
-            height={height}
-            onMouseDown={startDrawing}
-            onMouseUp={stopDrawing}
-            drawMode={drawMode}
-        />
+        <table
+            className='autotile squares'
+            onMouseDown={e => startDrawing(e)}
+            onMouseUp={e => stopDrawing(e)}
+        >
+            <tbody>
+                {tiles.map((row, y) => (
+                    <tr>
+                        {row.map((cell, x) => (
+                            <td
+                                style={{
+                                    backgroundPosition: cell.bitflag * 6.6 + '%'
+                                }}
+                                onClick={() => draw(x, y, 0)}
+                                onContextMenu={() => draw(x, y)}
+                                onMouseMove={drawMode !== null ? () => draw(x, y) : null}
+                            />
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
-}
 
-function Tile(props) {
-    // onClick={props.draw}
-    // onMouseMove={props.draw}
-    // onContextMenu={props.draw}
-
-    const [bitflag, setBitflag] = useState(0);
-
-    const setTile = t => {
-        setBitflag(t);
-    };
-
-    return (
-        <td
-            style={{ backgroundPosition: bitflag * (10 / 1.5) + '%' }}
-            onClick={() => setTile(props.drawMode !== 0)}
-            onContextMenu={() => setTile(props.drawMode === 0)}
-            onMouseMove={props.drawMode !== null && (() => setTile(props.drawMode === 0))}
-        />
-    );
+    // return (
+    //     <Table
+    //         width={width}
+    //         height={height}
+    //         tiles={tiles}
+    //         onMouseDown={e => startDrawing(e)}
+    //         onMouseUp={e => stopDrawing(e)}
+    //         onMouseMove={drawMode !== null ? draw : null}
+    //         draw={draw}
+    //     />
+    // );
 }
 
 function Table(props) {
@@ -61,11 +98,14 @@ function Table(props) {
     for (let y = 0; y < height; y++) {
         const row = [];
         for (let x = 0; x < width; x++) {
-            row.push(<Tile
+            const bitflag = (props.tiles[x + y * width] || {}).bitflag
+            row.push(<td
                 key={x + y * width}
                 index={x + y * width}
-                drawMode={props.drawMode}
-            />);
+                style={{ backgroundPosition: bitflag * 6.6 + '%' }}
+                onClick={props.draw}
+                onContextMenu={props.draw}
+            ></td>);
         }
         rows.push(<tr>{row}</tr>);
     }
